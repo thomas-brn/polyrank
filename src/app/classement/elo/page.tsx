56b9abc66a61ffacce622d4d86b22211e119f9ec
Thomas Broine <dev@thomasbroine.com>
2026-06-29 14:24:17 +0200

    chore: update .gitignore and modify Elo page title
    Removed specific migrations folder from .gitignore and updated the title on the Elo page to better reflect its content.

--- src/app/classement/elo/page.tsx
+++ src/app/classement/elo/page.tsx
@@ -12,7 +12,7 @@ export default async function EloPage() {
   return (
     <div className="flex flex-col gap-6">
       <PageHero
-        title="Comment fonctionne l'Elo ?"
+        title="Comment fonctionne le classement ?"
         description={`Le système de points qui classe les joueurs ${sport}.`}
       >
         <Link
